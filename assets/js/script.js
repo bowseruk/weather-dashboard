b4516772bc91739df84d1fc88e428b6e5841067e
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-02-06 22:55:56 +0000

    Added placeholders to the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,31 @@ fetch('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9
     .then((json) => console.log(json));
 
 let geourl = (cityName, stateCode, countryCode, limit, apiKey) => `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}${`,${stateCode}`}${`,${countryCode}`}&limit=${limit}&appid=${apiKey}`
+let weatherURL = (lat, lon, apiKey) => `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+
+function init() {
+    renderForecast()
+
+}
+
+function renderForecast() {
+    // Empty current contents
+    $('#today').empty()
+    $('#forecast').empty()
+
+    // Make the today box
+    $('#today').append($('<div>').append($('<h2>').text('Placeholder')).append($('<p>').text('Temp:')).append($('<p>').text('Wind:')).append($('<p>').text('Humidity:')))
+    // Forecast
+    for (let i = 0; i < 5; i++) {
+        $('#forecast').append($('<div>').append($('<h2>').text('Placeholder')).append($('<p>').text('Temp:')).append($('<p>').text('Wind:')).append($('<p>').text('Humidity:')))
+    }
+}
 
 $('#search-button').on('click', (event) => {
     event.preventDefault()
-    console.log(geourl('gaza', '','PS', 5, APIKEY))
+    console.log(geourl('Gainsborough', '','GB', 5, APIKEY))
+    console.log(weatherURL("53.3968736","-0.7739577",APIKEY));
 })
+
+init()
+
